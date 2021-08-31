2914ef30f97c6de4b5e031002799b0c64bef8dcf
Sobhan Dash <52308005+SobhanDash@users.noreply.github.com>
2021-08-31 12:11:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+import data from "./Data";
+
+function Card({ id, title, cal }) {
+  return (
+    <div className="card">
+      <h1>{title}</h1>
+      <p>you have consumed {cal} today</p>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        {data.map((item) => {
+          let { id, title, cal } = item;
+          return <Card key={id} title={title} cal={cal} />;
+        })}
+      </div>
+    </div>
+  );
+}
---
+++ src/Data.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 1,
+    title: "Pizza",
+    cal: 56
+  },
+  {
+    id: 2,
+    title: "Burger",
+    cal: 59
+  },
+  {
+    id: 3,
+    title: "Coke",
+    cal: 500
+  },
+  {
+    id: 4,
+    title: "Browne",
+    cal: 180
+  },
+  {
+    id: 5,
+    title: "Lasagna",
+    cal: 200
+  },
+  {
+    id: 1,
+    title: "Pani Puri",
+    cal: 20
+  }
+];
